cef003e545e9eef05c587201c8036f8a47ffdbe5
formingform <formingform.lyu@gmail.com>
2025-08-08 15:26:47 +0800

    fix type & init data

--- scripts/demo-init.sql
+++ scripts/demo-init.sql
@@ -1,9 +1,13 @@
+DROP TABLE  IF EXISTS `users`;
 CREATE TABLE `users` (
     `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
     `name` varchar(42) NOT NULL COMMENT '用户名',
-    `email` bigint(128) NOT NULL COMMENT 'email地址',
+    `email` varchar(128) NOT NULL COMMENT 'email地址',
     `create_time` datetime DEFAULT NULL COMMENT '创建时间',
     `update_time` datetime DEFAULT NULL COMMENT '修改时间',
     PRIMARY KEY (`id`),
     UNIQUE KEY `name` (`name`)
 );
+
+insert into `users` (`name`, `email`) values('Tom', 'tom@email.com');
+insert into `users` (`name`, `email`) values('Jerry', 'jerry@email.com');
