e9f71d9698a3e8d6f9b59399797330298471bf62
formingform <formingform.lyu@gmail.com>
2025-08-12 17:36:43 +0800

    update for docker compose

--- demo-init.sql
+++ demo-init.sql
@@ -8,8 +8,8 @@ CREATE TABLE `users` (
     `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
     `name` varchar(42) NOT NULL COMMENT '用户名',
     `email` varchar(128) NOT NULL COMMENT 'email地址',
-    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-    `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+    `create_time`            timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time`            timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     PRIMARY KEY (`id`),
     UNIQUE KEY `name` (`name`)
 );
